815cd941f9a62f7627e2e1f63e21bcbc40851da9
ruchou <joe04024@gapp.nthu.edu.tw>
2019-02-28 18:07:41 +0800

    add l2 assignment

--- L2/Test.py
+++ L2/Test.py
@@ -1,3 +1,81 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df_load = pd.read_csv('https://nthu-datalab.github.io/ml/labs/02_EDA_PCA/gen_dataset.csv')
+
+X_load = df_load.drop('Class label', 1)
+Y_load = df_load['Class label']
+
+df_load.head()
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# Z-normalize data
+sc = StandardScaler()
+Z = sc.fit_transform(X_load)
+# Estimate the correlation matrix
+R = np.dot(Z.T, Z) / df_load.shape[0]
+
+#calculate the eigen values, eigen vectors
+eigen_vals, eigen_vecs = np.linalg.eigh(R)
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eigen_pairs.sort(reverse=True)
+
+#form the projection matrix
+W_2D = np.hstack((eigen_pairs[0][1][:, np.newaxis],
+               eigen_pairs[1][1][:, np.newaxis]))
+
+#you should form a projection matrix which projects from raw-data dimension to 3 dimension here
 
 
-print('Hello World')
+W_3D = np.hstack((eigen_pairs[0][1][:, np.newaxis],
+               eigen_pairs[1][1][:, np.newaxis],
+                eigen_pairs[2][1][:, np.newaxis]
+                  ))
+
+
+
+
+
+import os
+import seaborn as sns
+sns.set(style='whitegrid', context='notebook')
+
+#import Axes3D for plottin 3d scatter
+from mpl_toolkits.mplot3d import Axes3D
+
+#cacculate z_pca(2d and 3d)
+Z_pca2 = Z.dot(W_2D)
+
+#plot settings
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+fig = plt.figure(figsize=(12,6))
+
+#plot 2D
+plt2 = fig.add_subplot(1,2,1)
+for l, c, m in zip(np.unique(Y_load), colors, markers):
+    plt2.scatter(Z_pca2[Y_load==l, 0],
+                Z_pca2[Y_load==l, 1],
+                c=c, label=l, marker=m)
+
+plt.title('Z_pca 2D')
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+
+#plot 3D
+plt3 = fig.add_subplot(1,2,2, projection='3d')
+#you should plot a 3D scatter using plt3.scatter here (see Axes3D.scatter in matplotlib)
+
+if not os.path.exists('./output'):
+    os.makedirs('./output')
+plt.savefig('./output/fig-pca-2-3-z.png', dpi=300)
+plt.show()
